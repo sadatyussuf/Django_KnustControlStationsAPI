619ab3cb360325256e3f0a86bd9d77cb265f94ce
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2021-10-23 15:38:59 +0000

    added csvfile

--- core/script.py
+++ core/script.py
@@ -8,10 +8,10 @@ path = "C:\\...."  # Set path of new directory here
 os.chdir(path)  # changing the directory
 
 User = get_user_model()
-
-with open('C:\\Users\\lenovo\\Desktop\\latlng.csv') as csvfile:  # Opening the latlng csv file
+# 'C:\\Users\\lenovo\\Desktop\\latlng.csv'
+with open('../latlng.csv') as csvfile:  # Opening the latlng csv file
     reader = csv.DictReader(csvfile)
     for row in reader:
-        p = ControlStation(name=row['Name'], latitude=row['latitude'], longitude=row['longitude'], x_northing=row['X'],
-                           y_easting=row['Y'], height=row['height'], description=row['Descprition'], poster=User.objects.first())
+        p = ControlStation(name=row['name'], latitude=row['latitude'], longitude=row['longitude'], x_northing=row['X'],
+                           y_easting=row['Y'], height=row['height'], description=row['description'], poster=User.objects.first())
         p.save()
